ec1f42cb50815dfdf8a056d8fc665df39e6c302a
zeyadomran <ziomran@gmail.com>
2021-06-08 17:01:16 +0400

    Added Favicon

--- client/src/pages/_document.tsx
+++ client/src/pages/_document.tsx
@@ -2,12 +2,15 @@ import NextDocument, { Head, Html, Main, NextScript } from "next/document";
 import { ColorModeScript } from "@chakra-ui/react";
 import theme from "../theme/theme";
 import React from "react";
+import { Favicon } from "../components/meta/favicon";
 
 export default class Document extends NextDocument {
 	render() {
 		return (
 			<Html lang="en">
-				<Head />
+				<Head>
+					<Favicon />
+				</Head>
 				<body>
 					{/* Make Color mode to persists when you refresh the page. */}
 					<ColorModeScript initialColorMode={theme.config.initialColorMode} />
---
+++ client/src/pages/index.tsx
@@ -0,0 +1,5 @@
+const Index: React.FC = () => {
+	return <h1>Index</h1>;
+};
+
+export default Index;
