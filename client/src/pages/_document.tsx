36a4b461431ab9c3587fd4063e7859acb1832fa3
zeyadomran <ziomran@gmail.com>
2021-06-10 01:44:04 +0400

    .

--- client/src/pages/_document.tsx
+++ client/src/pages/_document.tsx
@@ -1,13 +1,41 @@
 import NextDocument, { Head, Html, Main, NextScript } from "next/document";
 import React from "react";
-import { Favicon } from "../components/meta/Favicon";
 
 export default class Document extends NextDocument {
 	render() {
 		return (
 			<Html lang="en">
 				<Head>
-					<Favicon />
+					<link
+						rel="apple-touch-icon"
+						sizes="180x180"
+						href="images/apple-touch-icon.png"
+					/>
+					<link
+						rel="icon"
+						type="image/png"
+						sizes="32x32"
+						href="images/favicon-32x32.png"
+					/>
+					<link
+						rel="icon"
+						type="image/png"
+						sizes="16x16"
+						href="images/favicon-16x16.png"
+					/>
+					<link rel="manifest" href="images/site.webmanifest" />
+					<link
+						rel="mask-icon"
+						href="images/safari-pinned-tab.svg"
+						color="#41108e"
+					/>
+					<meta name="msapplication-TileColor" content="#41108e" />
+					<meta name="theme-color" content="#41108e" />
+					<link rel="preconnect" href="https://fonts.gstatic.com" />
+					<link
+						href="https://fonts.googleapis.com/css2?family=Bangers&family=Open+Sans&display=swap"
+						rel="stylesheet"
+					/>
 				</Head>
 				<body>
 					<Main />
